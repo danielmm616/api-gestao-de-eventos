5b209b9c3fe007f7a5541e1a7366456f900922ed
daniels <ddan.mm.16@gmail.com>
2022-06-23 20:34:48 -0300

    feat: base app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from "express";
+import { errorHandler } from "./errors/errors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err: Error, _: Request, res: Response, __: NextFunction) => {
+  return errorHandler(err, res);
+});
+
+export default app;
