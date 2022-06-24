4cd6d018d0b817fc8695f2f58070b0ad327939e3
daniels <ddan.mm.16@gmail.com>
2022-06-24 14:18:02 -0300

    feat: user entity with all relations

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,6 +1,14 @@
 import { compare } from "bcrypt";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Company } from "./company.entity";
+import { Event } from "./event.entity";
+import { Order } from "./order.entity";
 
 @Entity("users")
 export class User {
@@ -21,6 +29,12 @@ export class User {
 
   @ManyToOne(() => Company, (company) => company.employees, { eager: true })
   company: Company;
+
+  @ManyToOne(() => Event, (event) => event.users, { eager: true })
+  event: Event;
+
+  @OneToMany(() => Order, (order) => order.user)
+  orders?: Order[];
 
   comparePwd = async (pwd: string): Promise<boolean> => {
     return await compare(pwd, this.password);
