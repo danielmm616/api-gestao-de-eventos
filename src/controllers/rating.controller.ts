6660922e9dc8fc7aac06d7d916cf03778ceee57b
daniels <ddan.mm.16@gmail.com>
2022-06-28 12:21:08 -0300

    feat: rating controller already done

---
+++ src/controllers/rating.controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { ratingService } from "../services";
+
+class RatingController {
+  create = async (req: Request, res: Response) => {
+    const rating = await ratingService.create(req);
+
+    return res.status(201).json(rating);
+  };
+
+  getAll = async (req: Request, res: Response) => {
+    const ratings = await ratingService.getAll(req);
+
+    return res.status(200).json(ratings);
+  };
+
+  delete = async (req: Request, res: Response) => {
+    const rating = await ratingService.delete(req);
+
+    return res.status(200).json(rating);
+  };
+
+  update = async (req: Request, res: Response) => {
+    const rating = await ratingService.update(req);
+
+    return res.status(200).json(rating);
+  };
+}
+
+export default new RatingController();
