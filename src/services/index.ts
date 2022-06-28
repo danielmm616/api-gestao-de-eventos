1766ca802267322560c2c84b4cee4148eb38c837
daniels <ddan.mm.16@gmail.com>
2022-06-28 11:58:12 -0300

    update: services index imports and exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,13 @@
 import userService from "./user.service";
 import companyService from "./company.service";
+import eventService from "./event.service";
+import orderService from "./order.service";
+import ratingService from "./rating.service";
 
-export { companyService, userService };
+export {
+  companyService,
+  userService,
+  eventService,
+  orderService,
+  ratingService,
+};
