2812cc67f9c2a9deeff1bc6d3d2ddf2642236eca
daniels <ddan.mm.16@gmail.com>
2022-06-29 18:48:04 -0300

    update: data-source to suport tests

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,13 +5,21 @@ import path from "path";
 
 dotenv.config();
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  logging: false,
-  synchronize: true,
-  entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
-  migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
-});
+const AppDataSource = {
+  docker: new DataSource({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: false,
+    synchronize: true,
+    entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+    migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
+  }),
+  test: new DataSource({
+    type: "sqlite",
+    database: ":memory:",
+    entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+    synchronize: true,
+  }),
+};
 
-export default AppDataSource;
+export default AppDataSource[process.env.NODE_ENV];
