272192d14ab7568f78f95208e7d8f1b69d0acccf
daniels <ddan.mm.16@gmail.com>
2022-06-29 18:50:36 -0300

    reafact: cleaning unused code

--- src/repositories/invoice.repository.ts
+++ src/repositories/invoice.repository.ts
@@ -4,10 +4,6 @@ import { DeleteResult, Repository, UpdateResult } from "typeorm";
 
 interface IInvoiceRepo {
   save: (payload: Partial<Invoice>) => Promise<Invoice>;
-  retrieve: (payload: object) => Promise<Invoice>;
-  getAll: () => Promise<Invoice[]>;
-  update: (id: string, payload: object) => Promise<UpdateResult>;
-  delete: (id: string) => Promise<DeleteResult>;
 }
 
 class InvoiceRepository implements IInvoiceRepo {
@@ -18,16 +14,6 @@ class InvoiceRepository implements IInvoiceRepo {
   }
 
   save = async (payload: Partial<Invoice>) => await this.repo.save(payload);
-
-  retrieve = async (payload: object) =>
-    await this.repo.findOneBy({ ...payload });
-
-  getAll = async () => await this.repo.find();
-
-  update = async (id: string, payload: object) =>
-    await this.repo.update(id, { ...payload });
-
-  delete = async (id: string) => await this.repo.delete(id);
 }
 
 export default new InvoiceRepository();
